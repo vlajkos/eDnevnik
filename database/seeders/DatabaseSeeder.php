cd2d9ad71c39e1955bc6eed653097d24049d2c5e
Vladimir <vlajkic1453@gmail.com>
2023-03-31 23:48:27 +0200

    Prepravljena baza i modeli

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Profesor;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // $table->string("ime");
+        // $table->string("prezime");
+        // $table->string("email");
+        // $table->string("lozinka");
+        Profesor::create([
+            "ime" => "Nataša",
+            "prezime" => "Kostić",
+            "email" => "natsakostic77@gmail.com",
+            "lozinka" => "natasa293929",
+            "is_razredni" => false,
+
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
