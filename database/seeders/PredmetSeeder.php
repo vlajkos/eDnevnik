532907df76961b40c3208da7dc02c88641000f48
Vladimir <vlajkic1453@gmail.com>
2023-04-18 13:10:28 +0200

    Slovna greška

--- database/seeders/PredmetSeeder.php
+++ database/seeders/PredmetSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\predmet;
+use App\Models\Predmet;
 
 class PredmetSeeder extends Seeder
 {
